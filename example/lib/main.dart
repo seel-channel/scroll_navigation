1c0945a01fd12ef3adb35ac729187672baeb747a
= <=>
2020-08-12 15:16:22 -0500

    Example update with new features

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:scroll_navigation/scroll_navigation.dart';
 
@@ -6,6 +7,10 @@ void main() => runApp(App());
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+    ));
     return MaterialApp(title: 'Scroll Navigation Demo', home: HomePage());
   }
 }
@@ -19,78 +24,71 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return ScrollNavigation(
+      initialPage: 4,
       pages: <Widget>[
-        Screen(title: "Camera", body: Container(color: Colors.blue)),
-        Screen(title: "Messages", body: Container(color: Colors.green)),
-        Screen(
-          title: "Favorite",
-          body: Container(color: Colors.amber),
-          floatingButton: FloatingActionButton(
-            onPressed: () => print("Cool :)"),
-            child: Icon(Icons.add),
-          ),
-        ),
-        Screen(title: "Notifications", body: Container(color: Colors.yellow)),
-        Screen(title: "Home", body: Container(color: Colors.lightBlue)),
+        Screen(title: title("Camera"), body: Container(color: Colors.blue)),
+        Screen(title: title("Messages"), body: Container(color: Colors.green)),
+        Screen(title: title("Favorite"), body: Container(color: Colors.amber)),
+        Screen(title: title("Activity"), body: Container(color: Colors.yellow)),
+        Screen(title: title("Home"), body: Container(color: Colors.lightBlue)),
       ],
-      navItems: <BottomNavigationBarItem>[
-        navItem(Icons.camera),
-        navItem(Icons.chat),
-        navItem(Icons.favorite),
-        navItem(Icons.notifications),
-        navItem(Icons.home),
-      ],
-    );
-  }
-
-  BottomNavigationBarItem navItem(IconData icon) {
-    return BottomNavigationBarItem(
-      icon: Icon(icon),
-      title: Text(""),
-    );
-  }
-}
-
-//SCAFFOLD WITH CUSTOM APPBAR
-class Screen extends StatelessWidget {
-  final String title;
-  final Widget body, floatingButton;
-  Screen({Key key, this.title, this.body, this.floatingButton})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size(double.infinity, kToolbarHeight * 1.5),
-        child: Container(
-          height: MediaQuery.of(context).size.height * 0.1,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.3),
-                spreadRadius: -3,
-                blurRadius: 2,
-                offset: Offset(0, 3), // changes position of shadow
+      navItems: bottomNavItems(),
+      pagesActionButtons: <Widget>[
+        FloatingActionButton(
+          child: Icon(Icons.receipt),
+          backgroundColor: Colors.grey[600],
+          onPressed: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => Screen(
+                returnButton: true, //IMPORTANT TO RETURN!
+                title: title("New Page"),
+                body: Container(color: Colors.amber),
+                floatingButton: FloatingActionButton(
+                  onPressed: () => print("Cool :)"),
+                  child: Icon(Icons.add),
+                ),
               ),
-            ],
-          ),
-          child: Center(
-            child: Text(
-              title.toUpperCase(),
-              style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.grey[600],
-                  fontWeight: FontWeight.bold,
-                  wordSpacing: 1.5,
-                  letterSpacing: 0.5),
             ),
           ),
         ),
+        null,
+        null,
+        null,
+        DoubleFloatingIcon(
+          smallIcon: Icon(Icons.edit),
+          bigIcon: Icon(Icons.search),
+        ),
+      ],
+    );
+  }
+
+  List<BottomNavigationBarItem> bottomNavItems() {
+    return [
+      navItem(Icons.camera),
+      navItem(Icons.chat),
+      navItem(Icons.favorite),
+      navItem(Icons.notifications),
+      navItem(Icons.home),
+    ];
+  }
+
+  BottomNavigationBarItem navItem(IconData icon) {
+    return BottomNavigationBarItem(icon: Icon(icon), title: Text(""));
+  }
+
+  Widget title(String title) {
+    return Center(
+      child: Text(
+        title.toUpperCase(),
+        style: TextStyle(
+          fontSize: 22,
+          color: Colors.grey[600],
+          fontWeight: FontWeight.bold,
+          wordSpacing: 1.5,
+          letterSpacing: 0.5,
+        ),
       ),
-      body: body,
-      floatingActionButton: floatingButton,
     );
   }
 }
