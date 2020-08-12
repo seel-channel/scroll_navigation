42affbc19a82c93490f09ba6362b056f06116116
= <=>
2020-08-12 14:06:33 -0500

    Se privatizaron variables, se quito el appbar y sustiyo por la clase Screen

--- lib/scroll_navigation.dart
+++ lib/scroll_navigation.dart
@@ -13,16 +13,15 @@ class ScrollNavigation extends StatefulWidget {
     Key key,
     @required this.pages,
     @required this.navItems,
+    this.pagesActionButtons,
     this.initialPage = 0,
     this.showIdentifier = true,
     this.identifierPhysics = true,
+    this.identifierOnBottom = true,
     this.activeColor = Colors.blue,
     this.desactiveColor = Colors.grey,
     this.backgroundColorBody,
     this.backgroundColorNav = Colors.white,
-    this.backgroundColorAppBar = Colors.white,
-    this.appBarBrightnessLight = false,
-    this.appBar,
   }) : super(key: key);
 
   /// It is the initial page that will show. The value must match
@@ -32,9 +31,9 @@ class ScrollNavigation extends StatefulWidget {
   /// Are the pages that the Scroll Page will have
   final List<Widget> pages;
 
-  /// It will show an AppBar or any other PreferredSizeWidget.
-  /// Ignore the arguments: [backgroundColorNav] and [appBarBrightnessLight].
-  final PreferredSizeWidget appBar;
+  ///They are the floating action buttons or Widgets that the pages will have.
+  ///To separate pages you can put: Widget, null, Widget
+  final List<Widget> pagesActionButtons;
 
   /// It is the color that the active icon and indicator will show.
   final Color activeColor;
@@ -54,66 +53,72 @@ class ScrollNavigation extends StatefulWidget {
   /// If false, the argument [identifierPhysics] will be ignored
   final bool showIdentifier;
 
-  /// It is the color that the phone's status bar will have.
-  /// It is recommended that the color contrast with the background color of the AppBar
-  final bool appBarBrightnessLight;
+  ///Mostrará el identificador, en la parte inferior, si no en la parte superior
+  ///de la barra de navegación
+  final bool identifierOnBottom;
 
-  final Color backgroundColorNav, backgroundColorAppBar, backgroundColorBody;
+  final Color backgroundColorNav, backgroundColorBody;
 
   @override
-  _ScrollNavigationState createState() => _ScrollNavigationState();
+  ScrollNavigationState createState() => ScrollNavigationState();
 }
 
-class _ScrollNavigationState extends State<ScrollNavigation> {
-  int maxItemsCache = 24;
-  int bottomSelectedIndex;
-  List<int> popUpCache = List();
-  PageController pageController;
-  bool identifierPhysics, itemTapped = false;
-  Cubic animationCurve = Curves.linearToEaseOut;
-  Duration animationDuration = Duration(milliseconds: 400);
-  Map<String, double> identifier = {"position": 0.0, "width": 1.0};
-  Map<String, double> scroll = {"position": 0.0, "max": 0.0, "min": 1.0};
+class ScrollNavigationState extends State<ScrollNavigation> {
+  int _maxItemsCache = 5;
+  int _bottomSelectedIndex;
+  List<int> _popUpCache = List();
+  List<Widget> _pagesActionButtons = List();
+  PageController _pageController;
+  bool _identifierPhysics, _itemTapped = false;
+  Cubic _animationCurve = Curves.linearToEaseOut;
+  Duration _animationDuration = Duration(milliseconds: 400);
+  Map<String, double> _identifier = {"position": 0.0, "width": 1.0};
+  Map<String, double> _scroll = {"position": 0.0, "max": 0.0, "min": 1.0};
+
+  PageController get controller => _pageController;
 
   @override
   void initState() {
-    popUpCache.add(widget.initialPage);
-    bottomSelectedIndex = widget.initialPage;
-    identifierPhysics = widget.identifierPhysics;
-    pageController =
-        PageController(initialPage: widget.initialPage, keepPage: true);
-    if (widget.identifierPhysics && widget.showIdentifier)
-      pageController.addListener(scrollListener);
+    _popUpCache.add(widget.initialPage);
+    _bottomSelectedIndex = widget.initialPage;
+    _identifierPhysics = widget.identifierPhysics;
+
+    _pageController = PageController(initialPage: widget.initialPage);
+    if (_identifierPhysics && widget.showIdentifier) {
+      _pageController.addListener(_scrollListener);
+    }
+
+    if (widget.pagesActionButtons == null) {
+      _pagesActionButtons = List.filled(widget.pages.length, null);
+    } else {
+      for (var i = 0; i < widget.pages.length + 1; i++) {
+        i < widget.pagesActionButtons.length
+            ? _pagesActionButtons.add(widget.pagesActionButtons[i])
+            : _pagesActionButtons.add(null);
+      }
+    }
     super.initState();
   }
 
   @override
   void dispose() {
-    if (identifierPhysics && widget.showIdentifier)
-      pageController.removeListener(scrollListener);
+    if (_identifierPhysics && widget.showIdentifier)
+      _pageController.removeListener(_scrollListener);
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: onWillPop,
+      onWillPop: _onPressBackButton,
       child: Scaffold(
-        appBar: widget.appBar != null
-            ? widget.appBar
-            : PreferredSize(
-                preferredSize: Size.fromHeight(0.0),
-                child: AppBar(
-                  elevation: 0.0,
-                  backgroundColor: widget.backgroundColorAppBar,
-                  brightness: widget.appBarBrightnessLight
-                      ? Brightness.dark
-                      : Brightness.light,
-                ),
-              ),
-        body: buildPageView(context),
-        bottomNavigationBar: buildBottomNavigation(context),
+        body: PageView(
+            children: widget.pages,
+            controller: _pageController,
+            onPageChanged: (index) => _onChangePageIndex(index)),
         resizeToAvoidBottomPadding: false,
+        bottomNavigationBar: _buildBottomNavigation(context),
+        floatingActionButton: _pagesActionButtons[_bottomSelectedIndex],
         backgroundColor: widget.backgroundColorBody != null
             ? widget.backgroundColorBody
             : Colors.grey[100],
@@ -122,105 +127,104 @@ class _ScrollNavigationState extends State<ScrollNavigation> {
   }
 
   //FUNCIONES
-  void changePageIndex(int index) {
-    setState(() {
-      bottomSelectedIndex = index;
-      if (popUpCache.length > maxItemsCache) popUpCache.clear();
-      if (!itemTapped) popUpCache.add(index);
-    });
-  }
-
-  void bottomItemTapped(int index) async {
-    if (bottomSelectedIndex != index) {
-      setState(() {
-        itemTapped = true;
-        popUpCache.add(index);
-      });
-      await pageController.animateToPage(index,
-          duration: animationDuration, curve: animationCurve);
-      setState(() => itemTapped = false);
-    }
-  }
-
-  void scrollListener() {
-    setState(() {
-      scroll["position"] = pageController.position.pixels;
-      scroll["min"] = pageController.position.minScrollExtent;
-      scroll["max"] = pageController.position.maxScrollExtent;
-    });
-  }
-
-  Future<bool> onWillPop() async {
-    if (popUpCache.length != 1) {
-      popUpCache.removeLast();
-      bottomItemTapped(popUpCache[popUpCache.length - 1]);
-      popUpCache.removeLast();
+  Future<bool> _onPressBackButton() async {
+    if (_popUpCache.length != 1) {
+      _popUpCache.removeLast();
+      _onBottomItemTapped(_popUpCache[_popUpCache.length - 1]);
+      _popUpCache.removeLast();
       return false;
     } else
       return true;
   }
 
-  //WIDGETS
-  Widget buildPageView(BuildContext context) {
-    return PageView(
-      controller: pageController,
-      onPageChanged: (index) => changePageIndex(index),
-      children: widget.pages,
-    );
+  void _onChangePageIndex(int index) {
+    setState(() {
+      _bottomSelectedIndex = index;
+      if (_popUpCache.length > _maxItemsCache) _popUpCache.removeAt(0);
+      if (!_itemTapped) _popUpCache.add(index);
+    });
   }
 
-  Widget buildBottomNavigation(BuildContext context) {
+  void _onBottomItemTapped(int index) async {
+    if (_bottomSelectedIndex != index) {
+      setState(() {
+        _itemTapped = true;
+        _popUpCache.add(index);
+      });
+      await _pageController.animateToPage(
+        index,
+        duration: _animationDuration,
+        curve: _animationCurve,
+      );
+      setState(() => _itemTapped = false);
+    }
+  }
+
+  void _scrollListener() {
+    setState(() {
+      _scroll["position"] = _pageController.position.pixels;
+      _scroll["min"] = _pageController.position.minScrollExtent;
+      _scroll["max"] = _pageController.position.maxScrollExtent;
+    });
+  }
+
+  //WIDGETS
+  Widget _buildBottomNavigation(BuildContext context) {
     if (widget.showIdentifier) {
       double navWidth = MediaQuery.of(context).size.width;
       double itemLenght = 1 / widget.navItems.length;
       double identifierWidth = navWidth * itemLenght;
 
-      setState(() => identifier["width"] = identifierWidth);
+      setState(() => _identifier["width"] = identifierWidth);
 
-      if (identifierPhysics) {
+      if (_identifierPhysics) {
         double positionScale =
-            scroll["position"] / scroll["max"] - scroll["min"];
+            _scroll["position"] / _scroll["max"] - _scroll["min"];
         setState(() {
-          identifier["position"] = (navWidth - identifierWidth) * positionScale;
+          _identifier["position"] =
+              (navWidth - identifierWidth) * positionScale;
 
-          if (identifier["position"].isNaN)
-            identifier["position"] = identifierWidth * bottomSelectedIndex;
+          if (_identifier["position"].isNaN)
+            _identifier["position"] = identifierWidth * _bottomSelectedIndex;
         });
       } else {
         setState(() =>
-            identifier["position"] = identifierWidth * bottomSelectedIndex);
+            _identifier["position"] = identifierWidth * _bottomSelectedIndex);
       }
     }
-
     return Stack(
       children: <Widget>[
         BottomNavigationBar(
           showSelectedLabels: false,
           showUnselectedLabels: false,
-          currentIndex: bottomSelectedIndex,
+          currentIndex: _bottomSelectedIndex,
           selectedItemColor: widget.activeColor,
           unselectedItemColor: widget.desactiveColor,
           backgroundColor: widget.backgroundColorNav,
           type: BottomNavigationBarType.fixed,
-          onTap: (index) => bottomItemTapped(index),
+          onTap: (index) => _onBottomItemTapped(index),
           items: widget.navItems,
         ),
         if (widget.showIdentifier)
           AnimatedPositioned(
-            bottom: 0,
-            left: identifier["position"],
-            duration: identifierPhysics
+            bottom: widget.identifierOnBottom ? 0 : null,
+            left: _identifier["position"],
+            duration: _identifierPhysics
                 ? Duration(milliseconds: 100)
-                : animationDuration,
-            curve: animationCurve,
+                : _animationDuration,
+            curve: _animationCurve,
             child: Container(
               height: 3.0,
-              width: identifier["width"],
+              width: _identifier["width"],
               decoration: BoxDecoration(
                 color: widget.activeColor,
-                borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(10.0),
-                    topLeft: Radius.circular(10.0)),
+                borderRadius: widget.identifierOnBottom
+                    ? BorderRadius.only(
+                        topRight: Radius.circular(10.0),
+                        topLeft: Radius.circular(10.0))
+                    : BorderRadius.only(
+                        bottomRight: Radius.circular(10.0),
+                        bottomLeft: Radius.circular(10.0)),
               ),
             ),
           ),
