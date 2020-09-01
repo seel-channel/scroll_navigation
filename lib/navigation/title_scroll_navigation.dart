fdd4901d973b8bb4a9d795064d8a823a925ae103
= <=>
2020-08-31 23:06:51 -0500

    Se agrego el directionscaler

--- lib/navigation/title_scroll_navigation.dart
+++ lib/navigation/title_scroll_navigation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:scroll_navigation/misc/screen.dart';
 
 class TitleScrollNavigation extends StatefulWidget {
@@ -7,6 +8,12 @@ class TitleScrollNavigation extends StatefulWidget {
     @required this.titles,
     @required this.pages,
     this.initialPage = 0,
+    this.titleSize = 14.0,
+    this.paddingBetween = 10.0,
+    this.titleBold = true,
+    this.activeColor = Colors.blue,
+    this.desactiveColor = Colors.grey,
+    this.identifierColor = Colors.blue,
     this.backgroundColorBody,
     this.backgroundColorNav = Colors.white,
   }) : super(key: key);
@@ -19,6 +26,14 @@ class TitleScrollNavigation extends StatefulWidget {
   /// It is the initial page that will show. The value must match
   /// with the existing indexes and the total number of Nav Items
   final int initialPage;
+
+  final Color activeColor;
+  final Color desactiveColor;
+  final double paddingBetween;
+  final Color identifierColor;
+
+  final double titleSize;
+  final bool titleBold;
 
   final Color backgroundColorNav, backgroundColorBody;
 
@@ -34,6 +49,7 @@ class _TitleScrollNavigationState extends State<TitleScrollNavigation> {
   Duration _animationDuration = Duration(milliseconds: 400);
   Map<String, double> _identifier = {"position": 0.0, "width": 1.0};
   Map<String, double> _scroll = {"position": 0.0, "max": 0.0, "min": 1.0};
+  List<Map<String, double>> _titlesProps = List();
 
   @override
   void initState() {
@@ -47,10 +63,21 @@ class _TitleScrollNavigationState extends State<TitleScrollNavigation> {
   }
 
   void _scrollListener() {
+    ScrollDirection direction = _pageController.position.userScrollDirection;
+    double _scrollScale = _scroll["max"] / (widget.pages.length - 1);
+    double scaled =
+        (_scroll["position"] - (_scrollScale * _bottomSelectedIndex + 1)) /
+            _scrollScale;
+
     setState(() {
       _scroll["position"] = _pageController.position.pixels;
       _scroll["min"] = _pageController.position.minScrollExtent;
       _scroll["max"] = _pageController.position.maxScrollExtent;
+
+      if (direction == ScrollDirection.reverse)
+        _scroll["directionScaled"] = scaled > 0 ? scaled : scaled + 1;
+      else
+        _scroll["directionScaled"] = (scaled > 0 ? scaled : scaled + 1) * -1;
     });
   }
 
@@ -75,11 +102,12 @@ class _TitleScrollNavigationState extends State<TitleScrollNavigation> {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: minRow([
-        for (String title in widget.titles)
-          minRow([
-            Text(title),
-            SizedBox(width: 10),
-          ]),
+        ...widget.titles.map((title) {
+          return minRow([
+            Text(title, maxLines: 1),
+            SizedBox(width: widget.paddingBetween),
+          ]);
+        })
       ]),
     );
   }
