8f923157f52a055566560b7cda2951bfd9b17a89
= <=>
2020-09-02 19:46:18 -0500

    Se agrego documentacion

--- lib/misc/navigation_helpers.dart
+++ lib/misc/navigation_helpers.dart
@@ -4,8 +4,23 @@ class ScrollNavigationItem {
   /// Creates an item that is used with [ScrollNavigation.navItems].
   /// The argument [icon] should not be null and the argument [title] if null or empty don't show it.
   ScrollNavigationItem({@required this.icon, this.title, this.titleStyle});
+
+  ///The icon to display inside the button.
+  ///
+  /// {@tool snippet}
+  ///
+  ///The [Icon.color] will be ignored by the activeColor and desactiveColor of the ScrollNavigation.
+  ///
+  /// {@tool snippet}
+  ///
+  ///This property must not be null.
+  ///See [Icon], [ImageIcon].
   final Widget icon;
+
+  ///If the [title] is null or empty won't show it.
   final String title;
+
+  ///The style for title, the [color] property will ignored.
   final TextStyle titleStyle;
 }
 
@@ -49,6 +64,7 @@ class TitleScrollPadding {
   double left, right, top, bottom;
 }
 
+///Is used for Color Lerp effect with TextStyle hereded properties.
 TextStyle lerpTitleStyle({Color color, TextStyle style}) {
   style = style == null ? TextStyle() : style;
 
